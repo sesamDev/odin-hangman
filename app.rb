d2d434dfe7c1496c57c894dbd061b929542c9ddd
Sebastian <sebastian.samils@gmail.com>
2022-05-25 15:27:14 +0200

    Random word and displaying board
    Implemented choosing of random word from file and displaying an empty
    board based on the word.

--- app.rb
+++ app.rb
@@ -1,10 +1,30 @@
 lines = File.readlines('dict.txt')
 
-words = []
-lines.each do |line|
-  next unless line.length.between?(5, 12)
+def random_word(lines)
+  words = []
+  lines.each do |line|
+    next unless line.length.between?(5, 12)
 
-  words.push(line)
+    words.push(line)
+  end
+
+  words[rand(0..words.length - 1)]
 end
 
-puts words[rand(0..words.length - 1)]
+def update_board(word, guess)
+  word.each_char do |c|
+    print "#{guess.upcase} " if c == guess
+    print '_ '
+  end
+  puts ''
+end
+
+# incorrect_guesses = []
+# def display_incorrect_guess(guess)
+#   incor
+# end
+
+p word = random_word(lines) # remove print when game finished
+
+guess = gets.chomp
+update_board(word, guess)
